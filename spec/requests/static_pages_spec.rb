ef75e2cf79019a154d6b724fee68731f454bb8c3
mugisha <mugishaj@outlook.com>
2013-06-17 20:22:50 +0400

    replaced repetition in spec with let

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title){"Ruby on Rails Tutorial Sample App "}
   describe "Home page" do
     it "should have the content 'Sample App'" do
         visit '/static_pages/home'
@@ -9,7 +11,7 @@ describe "Static pages" do
 
     it "should have the right title" do 
         visit '/static_pages/home'
-        page.should have_selector('title', :text=> "Ruby on Rails Tutorial Sample App | Home")
+        page.should have_selector('title', :text=> "#{base_title}| Home")
     end
   end
 
@@ -21,7 +23,7 @@ describe "Static pages" do
  
     it "should have the right title" do
         visit "/static_pages/help"
-        page.should have_selector('title', :text=>"Ruby on Rails Tutorial Sample App | Help")
+        page.should have_selector('title', :text=>"#{base_title}| Help")
     end
   end
   describe "About page" do
@@ -32,7 +34,7 @@ describe "Static pages" do
 
     it "should have the right title" do
          visit '/static_pages/about'
-         page.should have_selector('title', :text=>"Ruby on Rails Tutorial Sample App | About")
+         page.should have_selector('title', :text=>"#{base_title}| About")
     end
   end
 
@@ -44,7 +46,7 @@ describe "Static pages" do
     
     it "should have the right title" do 
          visit '/static_pages/contact'
-         page.should have_selector('title', :title=>"Ruby on Rails Tutorial Sample App | About")  
+         page.should have_selector('title', :title=>"#{base_title}| About")  
     end
 
   end
